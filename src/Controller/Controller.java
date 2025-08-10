5f07f3d0e5736d1c6a1759f727106f3a05db4801
Gustavo Vaz Ferreira <gustavovaz278@gmail.com>
2025-08-10 17:08:57 -0300

    Atualização
    Atualização

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -1,18 +1,36 @@
 package Controller;
 
-public class Controller {
+import javax.swing.JOptionPane;
 
-	public void Metodo(int[] _Vetor) {
+public class Controller {
+	public static  int Tamanho() {
+		 int tamanho = Integer.parseInt(JOptionPane.showInputDialog("Digite o Tamanho do do Vetor"));
+		while (tamanho > 100 || tamanho < 1) {
+			tamanho = Integer.parseInt(JOptionPane.showInputDialog("O tamanho do vetor deve ser menor que 100"));
+
+		}
+		return tamanho;
+	}
+
+	public static int[] preencerVetor (int[] Vetor,int tamanho) {
+	for(int i = 0;i<tamanho;i++)
+	{
+		Vetor[i] = Integer.parseInt(JOptionPane.showInputDialog(" digite o valor " + (i + 1) + " do vetor."));
+
+	}return Vetor;
+	}
+
+	public void  Metodo(int[] _Vetor) {
 		for (int Vetor : _Vetor) {
 			if ((Vetor % 2) != 0) {
 
 				System.out.println(Vetor + " O Valor é Impar ");
 
 			} else if ((Vetor % 2) == 0) {
-				
+
 				int unidade = ((Vetor % 10));
-				if(unidade ==0) {
-				System.out.println(Vetor + " O Valor é Par e Multiplo de 10 ");
+				if (unidade == 0) {
+					System.out.println(Vetor + " O Valor é Par e Multiplo de 10 ");
 				}
 			}
 
--- src/view/Principal.java
+++ src/view/Principal.java
@@ -7,19 +7,13 @@ import Controller.Controller;
 public class Principal {
 	public static void main(String[] Args) {
 		Controller op = new Controller();
-		int tamanho = Integer.parseInt(JOptionPane.showInputDialog("Digite o Tamanho do do Vetor"));
+		int tamanho = op.Tamanho();
 
-		while (tamanho > 100 || tamanho < 1) {
-			tamanho = Integer.parseInt(JOptionPane.showInputDialog("O tamanho do vetor deve ser menor que 100"));
-
-		}
 		int[] Vetor = new int[tamanho];
-
-		for (int i = 0; i < tamanho; i++) {
-			Vetor[i] = Integer.parseInt(JOptionPane.showInputDialog(" digite o valor " + (i + 1) + " do vetor."));
-
-		}
+		op.preencerVetor(Vetor,tamanho);
+		
 		op.Metodo(Vetor);
 	}
 
 }
+// entrada 47, 12, 80, 59, 30, 71, 28, 90 
